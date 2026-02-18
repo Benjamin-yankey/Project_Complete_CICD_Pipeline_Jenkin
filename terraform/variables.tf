cf3df01bbbc3be49a2492fb4744f6e82f8be03dd
Benjamin Kofi Yankey <benjamin.yankey@amalitech.com>
2026-02-18 13:57:50 +0000

    changes

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -14,7 +14,7 @@ variable "environment" {
   description = "Environment name"
   type        = string
   default     = "dev"
-} 
+}
 
 variable "vpc_cidr" {
   description = "CIDR block for VPC"
@@ -23,7 +23,7 @@ variable "vpc_cidr" {
 }
 
 variable "public_subnets" {
-  description = "Public subnet CIDR blocks" 
+  description = "Public subnet CIDR blocks"
   type        = list(string)
   default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
